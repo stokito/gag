e25e603920e9f909972c24bc62224fb47e64f0d2
Sergey Ponomarev <stokito@gmail.com>
2012-05-26 12:15:03 +0300

    Restructure

---
+++ gag-annotations/src/main/java/com/google/gag/annotation/enforceable/Noop.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gag.annotation.enforceable;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Causes the annotated method or all the methods of the annotated class to
+ * become noop. Void methods do nothing; methods returning primitive numeric
+ * values return the equivalent of 0; methods returning boolean return false;
+ * methods returning objects return null.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({
+    ElementType.METHOD,
+    ElementType.CONSTRUCTOR,
+    ElementType.TYPE})
+public @interface Noop {
+  String value() default "";
+}
---
+++ gag-annotations/src/main/java/com/google/gag/enumeration/Source.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gag.enumeration;
+
+/**
+ * Quotation sources.
+ */
+public enum Source {
+  A_SPACE_ODYSSEY,
+  BATMAN,
+  BLADE_RUNNER,
+  BRAZIL,
+  BSG,
+  CTRL_ALT_DEL,
+  CYANIDE_AND_HAPPINESS,
+  DILBERT,
+  DR_STRANGELOVE,
+  HHGTTG,
+  HIJINKS_ENSUE,
+  LOTR,
+  MC_FRONTALOT,
+  MONTY_PYTHON,
+  MUNCHKINS,
+  OFFICE_SPACE,
+  PENNY_ARCADE,
+  PRINCESS_BRIDE,
+  QUESTIONABLE_CONTENT,
+  STARCRAFT,
+  STARGATE,
+  STAR_TREK,
+  STAR_TREK_TNG,
+  STAR_WARS,
+  THE_MATRIX,
+  TRON,
+  USER_FRIENDLY,
+  WE_THE_ROBOTS,
+  XKCD,
+  ZOMBIELAND,
+  OTHER
+}
